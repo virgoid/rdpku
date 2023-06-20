78dc13e6cf8845fa00fb854903ae144cde2cf943
mr virgo <virgoid.mail@gmail.com>
2023-06-19 19:12:58 -0700

    autoCommit 2023 0Mo.191258

--- run git.bat
+++ run git.bat
@@ -3,7 +3,7 @@ pushd "%userprofile%\Desktop\app"
 
 git add .
 git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
-git remote add origin https://github.com/virgoid/rdpku1.git
+@REM git remote add origin https://github.com/virgoid/rdpku1.git
 git push  origin main
 
 pause
