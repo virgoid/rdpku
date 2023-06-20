20f36baa9373e6bfc2d693662e839aef69342574
mr virgo <virgoid.mail@gmail.com>
2023-06-19 19:04:32 -0700

    initial commit --auto

--- run git.bat
+++ run git.bat
@@ -1,10 +1,10 @@
 
 pushd "%userprofile%\Desktop\app"
 
-for %%I in (.) do set dirName=%%~nxI
-curl -i -H "Authorization: token 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 " -d "{\"name\": \"%dirName%\", \"auto_init\": \"true\", \"private\": \"false\" }" https://api.github.com/user/repos
-git init
-echo>placeholder.txt
+@REM for %%I in (.) do set dirName=%%~nxI
+@REM curl -i -H "Authorization: token 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 " -d "{\"name\": \"%dirName%\", \"auto_init\": \"true\", \"private\": \"false\" }" https://api.github.com/user/repos
+@REM git init
+@REM echo>placeholder.txt
 git add .
 git commit -m "initial commit --auto"
 git remote add origin https://github.com/virgoid/%dirName%.git
