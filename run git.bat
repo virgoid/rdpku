06c754a280aacb34e76bb34edefec749ca5dcaa8
mr virgo <virgoid.mail@gmail.com>
2023-06-19 19:09:17 -0700

    initial commit --auto

--- run git.bat
+++ run git.bat
@@ -1,17 +1,11 @@
 
 pushd "%userprofile%\Desktop\app"
 
-@REM for %%I in (.) do set dirName=%%~nxI
-@REM curl -i -H "Authorization: token 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 " -d "{\"name\": \"%dirName%\", \"auto_init\": \"true\", \"private\": \"false\" }" https://api.github.com/user/repos
-@REM git init
-@REM echo>placeholder.txt
+
 git add .
 git commit -m "initial commit --auto"
 git remote add origin https://github.com/virgoid/rdpku1.git
-@REM git push -f --set-upstream origin master
-
-
- git push  origin main
+git push  origin main
 
 
 pause
