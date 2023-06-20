27b07ce7f720b4315b977800cd612c95ae5494af
mr virgo <virgoid.mail@gmail.com>
2023-06-19 18:35:41 -0700

    initial commit --auto

---
+++ run git.bat
@@ -0,0 +1,25 @@
+
+@REM pushd "%userprofile%\Desktop\app"
+
+for %%I in (.) do set dirName=%%~nxI
+curl -i -H "Authorization: token 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 " -d "{\"name\": \"%dirName%\", \"auto_init\": \"true\", \"private\": \"false\" }" https://api.github.com/user/repos
+git init
+echo>placeholder.txt
+git add .
+git commit -m "initial commit --auto"
+git remote add origin https://github.com/virgoid/%dirName%.git
+git push -f --set-upstream origin master
+
+
+
+@REM git add .
+@REM  git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+
+@REM  git branch -M main
+
+@REM git remote set-url origin https://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@github.com/virgoid/rdpku.git
+
+ git push origin main
+
+
+pause
