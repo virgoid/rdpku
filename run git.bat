9b72d8a1bbac716f17c468e7b6f939730ea1edea
mr virgo <virgoid.mail@gmail.com>
2023-06-19 19:02:28 -0700

    initial commit --auto

--- run git.bat
+++ run git.bat
@@ -11,15 +11,7 @@ git remote add origin https://github.com/virgoid/%dirName%.git
 @REM git push -f --set-upstream origin master
 
 
-
-@REM git add .
-@REM  git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
-
-@REM  git branch -M main
-
-@REM git remote set-url origin https://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@github.com/virgoid/rdpku.git
-
- git push -u origin main
+ git push  origin main
 
 
 pause
